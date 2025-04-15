46ceca171818e10a7c9be84f10557aefd9a701e4
c0a241540a <c0a241540a@edu.teu.ac.jp>
2025-04-15 17:06:17 +0900

    演習問題問題2こうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -15,22 +15,23 @@ def main():
     bf_img = pg.transform.flip(bg_img, True, False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-
+    kcl = [(-1,-1),(-1,1),(-2,0),(1,0),(-1,0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x = -tmr
         key_lst = pg.key.get_pressed()
+        kc = 4
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            kc = 0
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            kc = 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            kc = 2
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))
-        kk_rct.move_ip((-1,0)) 
+            kc = 3
+        kk_rct.move_ip(kcl[kc])
         screen.blit(bg_img, [x, 0])
         screen.blit(bf_img, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
